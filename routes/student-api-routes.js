9e38230c104cb5c5e2b4dc7a59f2fd3263ef727d
tleytek <lucidmat94@gmail.com>
2018-09-29 20:05:29 -0400

    student+teacher routes, student+teacher sqlize models

---
+++ routes/student-api-routes.js
@@ -0,0 +1,38 @@
+var db = require("../models");
+
+module.exports = function(app) {
+  // Get all teachers
+  app.get("/api/students", function(req, res) {
+    // Sequelize query to find all teachers, followed by a promise
+    db.Student.findAll({}).then(function(dbStudent) {
+      res.json(dbStudent);
+    });
+  });
+
+  // Create a new example
+  app.post("/api/students", function(req, res) {
+    db.Student.create(req.body).then(function(dbStudent) {
+      res.json(dbStudent);
+    });
+  });
+
+  // Delete an example by id
+  app.delete("/api/students/:id", function(req, res) {
+    db.Student.destroy({ where: { id: req.params.id } }).then(function(
+      dbStudent
+    ) {
+      res.json(dbStudent);
+    });
+  });
+};
+
+// PUT route for updating posts
+app.put("/api/students", function(req, res) {
+  db.Student.update(req.body, {
+    where: {
+      id: req.body.id
+    }
+  }).then(function(dbStudent) {
+    res.json(dbStudent);
+  });
+});
