bbce37c131c91ae67aaabd4da71a9c701a6d5847
Mateus Koehler <mateus@mateuskoehler.com>
2018-10-13 12:16:56 -0400

    getting data on to the dash, cleaning files

--- config/config.js
+++ config/config.js
@@ -1,24 +1,3 @@
-// {
-// 	"development": {
-// 		"username": "root",
-// 		"password": "password",
-// 		"database": "schooldb",
-// 		"host": "localhost",
-// 		"dialect": "mysql"
-// 	},
-// 	"test": {
-// 		"username": "root",
-// 		"password": null,
-// 		"database": "testdb",
-// 		"host": "localhost",
-// 		"dialect": "mysql",
-// 		"logging": false
-// 	},
-// 	"production": {
-// 		"use_env_variable": "JAWSDB_URL",
-// 		"dialect": "mysql"
-// 	}
-// }
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
